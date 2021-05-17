6cc817e839155ca3a07f7c3727c6dea094f346e2
Aun <aunsetthawut@gmail.com>
2021-05-17 12:45:21 +0700

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
+import Head from 'next/head';
+
+import '../styles/globals.css';
+import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,34 @@
-import { getFeaturedEvents } from "../duummy-data";
-import EventList from "../components/events/event-list";
+import { Fragment } from 'react';
+import Head from 'next/head';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+import FeaturedPosts from '../components/home-page/featured-posts';
+import Hero from '../components/home-page/hero';
+import { getFeaturedPosts } from '../lib/posts-util';
+
+function HomePage(props) {
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <Fragment>
+      <Head>
+        <title>Max' Blog</title>
+        <meta
+          name='description'
+          content='I post about programming and web development.'
+        />
+      </Head>
+      <Hero />
+      <FeaturedPosts posts={props.posts} />
+    </Fragment>
   );
+}
+
+export function getStaticProps() {
+  const featuredPosts = getFeaturedPosts();
+
+  return {
+    props: {
+      posts: featuredPosts,
+    },
+  };
 }
 
 export default HomePage;
